6d906acb405df5032a83507e899651ed32baa658
JS7542 <110913324+JS7542@users.noreply.github.com>
2026-07-31 16:48:54 +0900

    Add files via upload

---
+++ 07.31/sj03-start.sh
@@ -0,0 +1,27 @@
+# !/bin/bash
+# #########################
+# 반복문
+
+# for i in {1..10}; do
+#     echo "${i}"
+# done
+
+# #########################
+# COUNT=1
+# while [ ${COUNT} -le 10 ]; do
+#     echo "${COUNT}"
+#     COUNT=$(( ${COUNT} + 1 ))
+# done
+
+# #구구단
+# clear
+# for i in {1..9}; do
+#     RESULT=""
+#     for j in {2..9}; do
+#         MULT=$(( i * j ))
+#         SUM="${j} x ${i} = ${MULT}"
+#         RESULT="${RESULT}${SUM}\t"
+#     done
+#     echo -e "${RESULT}"
+# done
+
